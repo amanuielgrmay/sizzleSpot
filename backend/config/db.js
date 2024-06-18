0fcaf3cb4a54eca5ab684b2bc95e186c380be2f7
amanuielgrmay <amanuielgrmay@gmail.com>
2024-06-18 11:47:57 +0530

    route fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,6 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(`${mongodb}`)
+    .connect(process.env.MONGODB_SECRET_KEY)
     .then(() => console.log("db connected"));
 };
