23de72fd04fc9acb56a0f001fe8b69d086fcff1d
amanuielgrmay <66867848+amanuielgrmay@users.noreply.github.com>
2024-06-20 23:38:00 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,6 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect(
-      "mongodb+srv://amanuielgrmay:Ggpfep65@cluster0.nz30mbl.mongodb.net/sizzlespot"
-    )
+    .connect(process.env.SECRET_KEY)
     .then(() => console.log("db connected"));
 };
